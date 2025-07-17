68a3f4266ed1d95ce17f703bf7d69e453e3e2c71
player0307@wp.pl <player0307@wp.pl>
2025-07-18 01:26:17 +0200

    pushc changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+'use client'
+import styles from './styles.module.css'
+import ReactPlayer from 'react-player'
+import { useEffect, useState } from 'react'
+import { FaPlay } from 'react-icons/fa';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [timerDone, setTimerDone] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimerDone(true);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.overlay}>
+        <ReactPlayer
+          className={styles.video}
+          playsInline
+          src="/assets/overlay.mp4"
+          playing={true}
+          width="1920px"
+          height="1080px"
+          muted
+          loop={false}
+        />
+        <h1 className={timerDone ? styles.h1Active : styles.h1NoActive}>6 MONTHS!</h1>
+
+        {
+          timerDone && (
+            <div className={styles.videoMainWrapper}>
+              <ReactPlayer
+                className={styles.videoMain}
+                src="https://www.youtube.com/watch?v=pXdagO33j_w"
+                playing={isPlaying}
+                width="100%"
+                height="auto"
+                // autoPlay={isPlaying}
+                controls
+                loop={false}
+                light="/assets/thumbnail.jpg"
+                playIcon={
+                  <button className={styles.customPlayButton}>
+                    <FaPlay size={40} />
+                  </button>
+                }
+                onClickPreview={() => setIsPlaying(true)}
+              />
+            </div>
+          )
+        }
+        {
+          timerDone && <h2>M + S</h2>
+        }
+      </div>
     </div>
   );
 }
